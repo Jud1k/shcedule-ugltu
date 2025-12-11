279c34f2411eee3ba5829c0f447ec44db0e135f6
Jud1k <astronsahsa@mail.ru>
2025-12-11 20:47:28 +0500

    add update lesson

---
+++ frontend/src/features/lesson/api/update-lesson.ts
@@ -0,0 +1,44 @@
+import { MutationConfig } from '@/lib/react-query';
+import z from 'zod';
+import LessonService from './service';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useErrorHandler } from '@/hooks/useErrorHandler';
+
+export const updateLessonSchema = z.object({
+  time_id: z.string().min(1, 'Выберите время'),
+  day_of_week: z.string().min(1, 'Выберите день недели'),
+  type: z.string().min(1, 'Выберите тип'),
+  subject_id: z.string().min(1, 'Выберите предмет из списка'),
+  teacher_id: z.string().min(1, 'Выберите преподавателя из списка'),
+  room_id: z.string().min(1, 'Выберите аудиторию из списка'),
+  group_id: z.string().min(1, 'Выберите группу из списка'),
+});
+
+export type UpdateLessonForm = z.infer<typeof updateLessonSchema>;
+
+type UpdateLessonOptions = {
+  successMessage?: string;
+  mutationConfig?: MutationConfig<typeof LessonService.updateLesson>;
+};
+
+export const useUpdateLesson = ({
+  successMessage,
+  mutationConfig,
+}: UpdateLessonOptions) => {
+  const queryClient = useQueryClient();
+  const { handleApiError, handleSuccess } = useErrorHandler();
+
+  const { onSuccess, ...restConfig } = mutationConfig || {};
+  return useMutation({
+    onSuccess: (...args) => {
+      queryClient.invalidateQueries({ queryKey: ['lessons'] });
+      handleSuccess(successMessage || 'Пара успешно изменена');
+      onSuccess?.(...args);
+    },
+    onError: (error: unknown) => {
+      handleApiError(error);
+    },
+    ...restConfig,
+    mutationFn: LessonService.updateLesson,
+  });
+};
