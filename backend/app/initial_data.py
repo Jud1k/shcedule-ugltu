70d411bb059c10dd051b6a8c2df9599f4aba02eb
Jud1k <astronsahsa@mail.ru>
2025-12-17 19:16:27 +0500

    add creating building in initial_data

--- backend/app/initial_data.py
+++ backend/app/initial_data.py
@@ -1,10 +1,12 @@
 import asyncio
 from app.domain.auth.utils import get_password_hash
+from app.domain.building.repository import BuildingRepository
+from app.domain.building.schemas import BuildingCreate
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.database import engine
-from app.db.models import User
+from app.db.models import Building, User
 from app.core.config import settings
 from app.domain.auth.repository import UserRepository
 from app.domain.auth.schemas import UserCreate
@@ -19,10 +21,15 @@ async def init():
             user_in = UserCreate(
                 email=settings.FIRST_SUPERUSER, password=new_password, role="admin"
             )
-            repo = UserRepository(session)
-            await repo.create(user_in)
-            await session.commit()
-
+            user_repo = UserRepository(session)
+            await user_repo.create(user_in)
+        building_in = BuildingCreate(name="ГУК",address="Сибирский тракт 38")
+        result = await session.execute(select(Building).where(Building.name==building_in.name))
+        building = result.scalar_one_or_none()
+        if not building:
+            building_repo = BuildingRepository(session)
+            await building_repo.create(building_in)
+        await session.commit()
 
 async def main():
     print("Creating initial data")
